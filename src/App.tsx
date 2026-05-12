c6eeba718c7e7b2a9b2849a5035bb728deec6c9a
lgcarlinf <luiggylml@gmail.com>
2026-05-12 00:58:26 -0500

    refactor: lazy load ScrollReveal for improved performance

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,30 @@ import "./scss/App.scss";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import ThemeContext from "./context/ThemeProvider";
-import ScrollReveal from "scrollreveal";
 import { THEME } from "./types";
 
 function App() {
   const { theme } = use(ThemeContext);
 
-  
   useEffect(() => {
-    const sr = ScrollReveal({
-      origin: "top",
-      distance: "60px",
-      duration: 2500,
-      delay: 400,
-    });
+    import("scrollreveal").then(({ default: ScrollReveal }) => {
+      const sr = ScrollReveal({
+        origin: "top",
+        distance: "60px",
+        duration: 2500,
+        delay: 400,
+      });
 
-    sr.reveal(`.profile__border`);
-    sr.reveal(`.change-theme`);
-    sr.reveal(`.profile__name`, { delay: 500 });
-    sr.reveal(`.profile__profession`, { delay: 600 });
-    sr.reveal(`.profile__social`, { delay: 700 });
-    sr.reveal(`.profile__info-group`, { interval: 100, delay: 700 });
-    sr.reveal(`.profile__buttons`, { delay: 800 });
-    sr.reveal(`.filters__content`, { delay: 900 });
-    sr.reveal(`.filters`, { delay: 1000 });
+      sr.reveal(`.profile__border`);
+      sr.reveal(`.change-theme`);
+      sr.reveal(`.profile__name`, { delay: 500 });
+      sr.reveal(`.profile__profession`, { delay: 600 });
+      sr.reveal(`.profile__social`, { delay: 700 });
+      sr.reveal(`.profile__info-group`, { interval: 100, delay: 700 });
+      sr.reveal(`.profile__buttons`, { delay: 800 });
+      sr.reveal(`.filters__content`, { delay: 900 });
+      sr.reveal(`.filters`, { delay: 1000 });
+    });
   }, []);
 
   return (
